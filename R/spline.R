7d5123d12407e2d2c7b02258b26d969bf1067f22
gravesti <isaac.gravestock@roche.com>
2022-05-24 07:40:44 +0000

    add add_splines_df

--- R/spline.R
+++ R/spline.R
@@ -75,3 +75,69 @@ add_splines <- function(data_path, out_path = data_path, period_spline, followup
     return_splines
   }
 }
+
+
+
+#' Add splines to expanded data in memory
+#'
+#' Add natural spline basis columns to the expanded data for period and follow up time.
+#'
+#' @param data A `data.table` containing the expanded data.
+#' @param period_spline Named list of arguments for `ns` for periods spline (column `for_period`)
+#' @param followup_spline Named list of arguments for `ns` for follow up time (column `follow_time`)
+#'
+#' @return Returns a data.table with the splines columns added.
+#' @export
+#' @import assertthat
+#'
+add_splines_df <- function(data, period_spline, followup_spline){
+
+  data <- as.data.table(data)
+  assert_that(is.data.table(data))
+
+  attempt_period <- attempt_followup <- FALSE
+
+  attempt_period <- if (!missing(period_spline)){
+    if (any(!is.na(period_spline))){
+      assert_that(is.list(period_spline), msg = "period_spline is not a list of arguments for ns()")
+      TRUE
+    }
+  }
+
+  attempt_followup <- if (!missing(followup_spline)){
+    if (any(!is.na(followup_spline))){
+      assert_that(is.list(followup_spline), msg = "followup_spline is not a list of arguments for ns()")
+      TRUE
+    }
+  }
+
+  # Check columns exist in data
+  if(isTRUE(attempt_period)) {
+    assert_that("for_period" %in% colnames(data),
+                msg = "Spline specified for period but data does not have for_period column.")
+  }
+
+  if(isTRUE(attempt_followup)) {
+    assert_that("followup_time" %in% colnames(data),
+                msg = "Spline specified for follow-up time but data does not have followup_time column.")
+  }
+
+  # Create splines for the followup_time and for_period as required.
+  if(isTRUE(attempt_period) | isTRUE(attempt_followup)){
+
+    if(isTRUE(attempt_period)){
+      temp <- do.call("ns", c(x = list(data[["for_period"]]), period_spline))
+      for(i in 1:ncol(temp)){
+        data[, paste0("period_base_", i)] <- temp[, i]
+      }
+    }
+
+    if(isTRUE(attempt_followup)){
+      temp <- do.call("ns", c(x = list(data[["followup_time"]]), followup_spline))
+      for(i in 1:ncol(temp)){
+        data[, paste0("followup_base_", i)] <- temp[, i]
+      }
+    }
+  }
+  data
+}
