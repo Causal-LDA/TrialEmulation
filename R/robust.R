1ba2376decf9ade57d12d72661c86c3a076e6162
gravesti <isaac.gravestock@roche.com>
2022-06-17 14:53:46 +0200

    update tests, data description and code style

--- R/robust.R
+++ R/robust.R
@@ -1,5 +1,5 @@
 
-#' Robust Calculation Function
+#' Robust Variance Calculation
 #'
 #' This function performs the calculation of robust standard errors based on
 #' variances estimated using [`sandwich::vcovCL`].
@@ -9,24 +9,30 @@
 #'
 #' @returns A list with elements `summary`, a table with the model summary using the
 #' robust variance estimates, and `matrix`, the `sandwich` covariance matrix.
-
-robust_calculation <- function(model, data_id){
+#'
+robust_calculation <- function(model, data_id) {
   # Dummy variables used in data.table calls declared to prevent package check NOTES:
   lb <- estimate <- robust_se <- ub <- z <- p_value <- NULL
 
-  est_temp = model$coefficients
-  v = sandwich::vcovCL(model, cluster = data_id,
-                       type = NULL, sandwich = TRUE, fix = FALSE)
+  est_temp <- model$coefficients
+  v <- sandwich::vcovCL(
+    model,
+    cluster = data_id,
+    type = NULL,
+    sandwich = TRUE,
+    fix = FALSE
+  )
 
-  se = sqrt(diag(v))
-  output <- data.table(names = names(model$coefficients),
-                       estimate = est_temp,
-                       robust_se = se[names(est_temp)])
-
+  se <- sqrt(diag(v))
+  output <- data.table(
+    names = names(model$coefficients),
+    estimate = est_temp,
+    robust_se = se[names(est_temp)]
+  )
 
   output[, lb := estimate - (1.96 * robust_se)]
   output[, ub := estimate + (1.96 * robust_se)]
-  output[, z := estimate/robust_se]
-  output[, p_value := format.pval(2*(1-pnorm(abs(z))), eps=0.001)]
+  output[, z := estimate / robust_se]
+  output[, p_value := format.pval(2 * (1 - pnorm(abs(z))), eps = 0.001)]
   return(list(summary = output, matrix = v))
 }
--- tests/testthat/test-modelling.R
+++ tests/testthat/test-modelling.R
@@ -5,7 +5,7 @@ test_that("data_modelling can be quiet", {
   data.table::fwrite(vignette_switch_data, data_path)
   expect_silent(
     result <- data_modelling(
-      outcomeCov_var = c("catvarA", "catvarB", "catvarC","nvarA","nvarB","nvarC"),
+      outcomeCov_var = c("catvarA", "catvarB", "catvarC", "nvarA", "nvarB", "nvarC"),
       model_var = "assigned_treatment",
       include_followup_time_case = "linear",
       include_expansion_time_case = "linear",
@@ -22,7 +22,7 @@ test_that("data_modelling gives expected results in example data", {
   data_path <- file.path(working_dir, "switch_data.csv")
   data.table::fwrite(vignette_switch_data, data_path)
   result <- data_modelling(
-    outcomeCov_var = c("catvarA", "catvarB", "catvarC","nvarA","nvarB","nvarC"),
+    outcomeCov_var = c("catvarA", "catvarB", "catvarC", "nvarA", "nvarB", "nvarC"),
     model_var = "assigned_treatment",
     include_followup_time_case = "linear",
     include_expansion_time_case = "linear",
@@ -32,15 +32,18 @@ test_that("data_modelling gives expected results in example data", {
   )
   unlink(data_path)
   expect_class(result$model, "glm")
-  expected_coefs <- c(`(Intercept)` = -6.17184262601338, assigned_treatment = -0.118265495621331,
-                      for_period = 0.00325072039757488, followup_time = 0.00168035277357471)
+  expected_coefs <- c(
+    `(Intercept)` = -6.17184262601338, assigned_treatment = -0.118265495621331,
+    for_period = 0.00325072039757488, followup_time = 0.00168035277357471
+  )
   expect_equal(result$model$coefficients, expected_coefs)
 
-
   expect_equal(
-    result$robust$names,
+    result$robust$summary$names,
     c("(Intercept)", "assigned_treatment", "for_period", "followup_time")
   )
   expected_robust_se <- c(0.34731920891695, 0.293437149718051, 0.00130047600033232, 0.00155479427447236)
-  expect_equal(result$robust$robust_se, expected_robust_se)
+  expect_equal(result$robust$summary$robust_se, expected_robust_se)
+
+  expect_matrix(result$robust$matrix, nrows = 4, ncols = 4, any.missing = FALSE)
 })
