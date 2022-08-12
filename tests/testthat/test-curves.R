bbac511eedbd85e6144947311d8ed6358d7f4a85
gravesti <isaac.gravestock@roche.com>
2022-08-12 17:12:42 +0200

    test skeleton

--- tests/testthat/test-curves.R
+++ tests/testthat/test-curves.R
@@ -1,3 +1,19 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("h_extract_baseline works", {
+
+})
+
+test_that("predict_survival", {
+
+})
+
+test_that("sum_up_ci", {
+
+})
+
+test_that("ci_up_to", {
+
+})
+
+test_that("survival_up_to", {
+
 })
