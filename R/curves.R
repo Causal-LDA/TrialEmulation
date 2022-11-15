4535a55b2ef8ebfbf1409d0b9a13fdf432dae1ed
gravesti <isaac.gravestock@roche.com>
2022-11-15 14:59:53 +0100

    big update

--- R/curves.R
+++ R/curves.R
@@ -5,15 +5,15 @@
 #' @param object Object from [data_modelling()] or [initiators()].
 #' @param newdata Baseline trial data to predict cumulative incidence or survival for. If `newdata` contains
 #' rows with `followup_time > 0` these will be removed.
-#' @param type Type of values to calculate. The default is cumulative incidence (`"cum_inc"`). Other options may be
-#'  supported in future.
+#' @param type Type of values to calculate. Either cumulative incidence (`"cum_inc"`) or survival (`"survival"`).
 #' @param predict_times Follow-up times to predict. Any times given in newdata will be ignored.
 #' @param conf_int Calculate a confidence interval using coefficient samples from a multivariate normal distribution
 #' based on the robust covariance matrix.
 #' @param samples The number of samples of the coefficients for prediction models.
 #' @param ... Further arguments passed to or from other methods.
 #'
-#' @return A list of two data frames containing the cumulative incidences for each of the assigned treatment options.
+#' @return A list of three data frames containing the cumulative incidences for each of the assigned treatment options
+#'  and the difference between them.
 #' @export
 #' @importFrom stats .checkMFClasses coef delete.response model.frame model.matrix terms
 #' @examples
@@ -51,7 +51,7 @@ predict.RTE_model <- function(object,
                               predict_times,
                               conf_int = TRUE,
                               samples = 100,
-                              type = c("cum_inc", "survival", "response"),
+                              type = c("cum_inc", "survival"),
                               ...) {
   assert_class(object$model, "glm")
   model <- object$model
@@ -76,8 +76,6 @@ predict.RTE_model <- function(object,
     calculate_survival
   } else if (type == "cum_inc") {
     calculate_cum_inc
-  } else if (type == "response") {
-    function(x) x
   }
 
   pred_list <- lapply(
@@ -89,21 +87,22 @@ predict.RTE_model <- function(object,
     linkinv = model$family$linkinv,
     matrix_n_col = length(predict_times)
   )
-
-  if (type %in% c("cum_inc", "survival")) {
-    pred_list$difference <- pred_list$assigned_treatment_1 - pred_list$assigned_treatment_0
-  }
+  pred_list$difference <- pred_list$assigned_treatment_1 - pred_list$assigned_treatment_0
 
   mapply(
     pred_matrix = pred_list,
-    col_names = if (type == "response") rep(type, 2) else paste0(type, c("", "", "_diff")),
+    col_names = paste0(type, c("", "", "_diff")),
     SIMPLIFY = FALSE,
     FUN = function(pred_matrix, col_names) {
-      quantiles <- apply(pred_matrix, 1, quantile, probs = c(0.025, 0.975))
-      setNames(
-        data.frame(predict_times, pred_matrix[, 1], quantiles[1, ], quantiles[2, ]),
-        c("followup_time", col_names, "2.5%", "97.5%")
-      )
+      if (conf_int) {
+        quantiles <- apply(pred_matrix, 1, quantile, probs = c(0.025, 0.975))
+        setNames(
+          data.frame(predict_times, pred_matrix[, 1], quantiles[1, ], quantiles[2, ]),
+          c("followup_time", col_names, "2.5%", "97.5%")
+        )
+      } else {
+        setNames(data.frame(predict_times, pred_matrix[, 1]), c("followup_time", col_names))
+      }
     }
   )
 }
@@ -143,114 +142,12 @@ check_newdata <- function(newdata, model, predict_times) {
   newdata
 }
 
-#' Predict Cumulative Incidence
-#'
-#' @param object The result from `initiators`. Either object or model must be specified.
-#' @param model A `glm` object.
-#' @param predict_times a vector of follow up times to predict values for.
-#' @param newdata `data.frame` to predict survival for. Extracted from `model`/`object` if not provided.
-#'
-#' @return a list with estimated cumulative incidence curves for `assigned_treatment`
-#' equal to 0 and 1.
-#' @export
-#' @importFrom stats predict
-#'
-#' @examples
-#' data("trial_example")
-#' i <- initiators(
-#'   data = trial_example,
-#'   id = "id",
-#'   period = "period",
-#'   eligible = "eligible",
-#'   treatment = "treatment",
-#'   outcome = "outcome",
-#'   outcome_cov = ~ nvarA + nvarB,
-#'   include_followup_time_case = ~followup_time,
-#'   include_expansion_time_case = ~for_period,
-#'   last_followup = 30,
-#'   last_period = 30,
-#'   use_censor = 0,
-#'   use_weight = 0
-#' )
-#'
-#' predict_cum_inc(i, predict_times = seq(1, 20, by = 2))
-#'
-predict_cum_inc <- function(object, model, predict_times, newdata) {
-  if (missing(model) && missing(object)) stop("Either model or object must be specified.")
-  if (missing(model)) model <- object$model
-  assert_class(model, "glm")
-  assert_numeric(predict_times, lower = 0, min.len = 1, any.missing = FALSE)
-
-  # Dummy assignments for data.table
-  assigned_treatment <- NULL
-
-  if (missing(newdata)) newdata <- as.data.table(model$data)
-
-  baseline <- newdata[newdata$followup_time == 0, ]
-  expanded <- baseline[rep(seq_len(nrow(baseline)), times = length(predict_times)), ]
-  expanded$followup_time <- rep(predict_times, each = nrow(baseline))
-
-  pred_0 <- predict(model, newdata = expanded[, assigned_treatment := 0], type = "response")
-  pred_1 <- predict(model, newdata = expanded[, assigned_treatment := 1], type = "response")
-
-  preds <- expanded[, c("id", "followup_time", "for_period")]
-  preds$pred_0 <- pred_0
-  preds$pred_1 <- pred_1
-  setorderv(preds, cols = c("for_period", "followup_time", "id"))
-
-  pred_trt <- list(
-    trt_0 = tapply(preds$pred_0, preds$for_period, matrix, ncol = length(predict_times)),
-    trt_1 = tapply(preds$pred_1, preds$for_period, matrix, ncol = length(predict_times))
-  )
-
-  lapply(pred_trt, sum_up_cum_inc)
-}
-
-
-#' Helper to Calculate Cumulative Incidence
-#'
-#' @param p_mat_list A list of probability matrices with rows for each subject and followup time as the columns.
-#' All matrices must have the followup times (i.e. same number of columns).
-#'
-#' @return A vector of cumulative incidences.
-#' @export
-#'
-#' @examples
-#' surv_prob_list <- list(
-#'   trial_1 = matrix(
-#'     c(
-#'       0.1, 0.1, 0.1,
-#'       0.5, 0.2, 0.1
-#'     ),
-#'     nrow = 2, byrow = TRUE
-#'   ),
-#'   trial_2 = matrix(
-#'     c(
-#'       0.15, 0.15, 0.15,
-#'       0.45, 0.25, 0.1
-#'     ),
-#'     nrow = 2, byrow = TRUE
-#'   )
-#' )
-#' sum_up_cum_inc(surv_prob_list)
-sum_up_cum_inc <- function(p_mat_list) {
-  cols <- unique(vapply(p_mat_list, ncol, integer(1L)))
-  assert_integer(cols, len = 1, lower = 1)
-
-  cum_inc_mat <- vapply(p_mat_list, cum_inc_up_to, numeric(cols))
-  total_n <- sum(vapply(p_mat_list, nrow, integer(1L)))
-  result <- rowSums(cum_inc_mat) / total_n
-  assert_monotonic(result)
-  result
-}
-
 
 #' Calculate Cumulative Incidence and Survival
 #'
 #' @param p_mat Probability matrix with rows for each subject and follow-up time as the columns.
 #'
 #' @return A vector containing the cumulative incidence or survival values.
-#' @export
 #'
 #' @examples
 #' surv_prob <- matrix(
@@ -261,36 +158,7 @@ sum_up_cum_inc <- function(p_mat_list) {
 #'   nrow = 2,
 #'   byrow = TRUE
 #' )
-#' cum_inc_up_to(surv_prob)
-cum_inc_up_to <- function(p_mat) {
-  assert_matrix(p_mat, mode = "numeric")
-
-  prod_term <- apply(1 - cbind(0, p_mat)[, -ncol(p_mat), drop = FALSE], 1, cumprod)
-  sum_term <- prod_term * t(p_mat)
-  cumsum_term <- apply(sum_term, 2, cumsum)
-  result <- rowSums(cumsum_term)
-  assert_monotonic(result)
-  result
-}
-
-
-#' @rdname cum_inc_up_to
-#' @export
-survival_up_to <- function(p_mat) {
-  assert_matrix(p_mat, mode = "numeric")
-  result <- c(1, rowMeans(apply(1 - p_mat, 1, cumprod)))
-  assert_monotonic(result, increasing = FALSE)
-  result
-}
-
-
-calculate_survival <- function(p_mat) {
-  assert_matrix(p_mat, mode = "numeric")
-  result <- rowMeans(apply(1 - p_mat, 1, cumprod))
-  assert_monotonic(result, increasing = FALSE)
-  result
-}
-
+#' calculate_cum_inc(surv_prob)
 calculate_cum_inc <- function(p_mat) {
   assert_matrix(p_mat, mode = "numeric")
   result <- 1 - calculate_survival(p_mat)
@@ -298,6 +166,29 @@ calculate_cum_inc <- function(p_mat) {
   result
 }
 
+#' @rdname calculate_cum_inc
+#' @examples
+#' calculate_survival(surv_prob)
+calculate_survival <- function(p_mat) {
+  assert_matrix(p_mat, mode = "numeric")
+  result <- rowMeans(apply(1 - p_mat, 1, cumprod))
+  assert_monotonic(result, increasing = FALSE)
+  result
+}
+
+
+
+#' Calculate and transform predictions
+#'
+#' @param model_matrix Model matrix to multiple with coefficients
+#' @param treatment_value Value to insert into `assigned_treatment` column
+#' @param pred_fun Function to transform prediction matrix
+#' @param coefs_mat Matrix of coefficients corresponding to `model_matrix`.
+#' @param linkinv Inverse link function for transforming linear predictor
+#' @param matrix_n_col Expected number of column after prediction.
+#'
+#' @return A matrix with transformed predicted values. Number of columns corresponds
+#'  to the number of rows of `coefs_mat`
 calculate_predictions <- function(model_matrix, treatment_value, pred_fun, coefs_mat, linkinv, matrix_n_col) {
   treatment_col <- which(colnames(model_matrix) == "assigned_treatment")
   model_matrix[, treatment_col] <- treatment_value
