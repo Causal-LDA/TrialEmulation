871df17955e36244c89d8efa2f2165bd1056ab6c
Li Su <37799312+lisu-stats@users.noreply.github.com>
2024-01-11 15:23:09 +0000

    Update documentation for predict()

--- R/predict.R
+++ R/predict.R
@@ -1,4 +1,11 @@
-#' Predict Marginal Cumulative Incidences with Confidence Intervals for a Target Trial Population
+#' Predict marginal vumulative incidences with confidence intervals for a target trial population
+#'
+#' This function predicts the marginal cumulative incidences when a target trial population receives 
+#' either the treatment or non-treatment at baseline (for an intention-to-treat analysis) or either 
+#' sustained treatment or sustained non-treatment (for a per-protocol analysis). The difference between
+#' these cumulative incidences is the estimated causal effect of treatment. Currently, the `predict` function 
+#' only provides marginal intention-to-treat and per-protocol effects, therefore it is only valid when 
+#' `estimand_type = "ITT"` or `estimand_type = "PP"`.
 #'
 #' @param object Object from [trial_msm()] or [initiators()].
 #' @param newdata Baseline trial data that characterise the target trial population that marginal cumulative incidences
@@ -9,9 +16,9 @@
 #'   (`"cum_inc"`) or survival probability (`"survival"`).
 #' @param predict_times Specify the follow-up visits/times where the marginal cumulative incidences or survival
 #'   probabilities are predicted.
-#' @param conf_int Estimate the point-wise 95\% confidence intervals of cumulative incidences for the target trial
+#' @param conf_int Construct the point-wise 95-percent confidence intervals of cumulative incidences for the target trial
 #'   population under treatment and non-treatment and their differences by simulating the parameters in the marginal
-#'   structural model from a multivariate normal distribution with mean equal to the marginal structural model parameter
+#'   structural model from a multivariate normal distribution with the mean equal to the marginal structural model parameter
 #'   estimates and the variance equal to the estimated robust covariance matrix.
 #' @param samples Number of samples used to construct the simulation-based confidence intervals.
 #' @param ... Further arguments passed to or from other methods.
