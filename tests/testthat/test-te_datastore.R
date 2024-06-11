2f0ba5796eaf45f189175b67ec1508c4abd8d319
darkgoldenrod-cherry <julia.moesch@roche.com>
2024-06-11 11:41:57 +0200

    read_expanded_data method for te_datastore_duckdb objects added, tests added, documentation adjusted

--- tests/testthat/test-te_datastore.R
+++ tests/testthat/test-te_datastore.R
@@ -26,3 +26,31 @@ test_that("read_expanded_data can read te_datastore_csv data", {
 
   unlink(temp_dir, recursive = TRUE)
 })
+
+
+test_that("read_expanded_data can read te_datastore_duckdb data", {
+  temp_dir <- tempfile("duckdb_dir_")
+  dir.create(temp_dir)
+  datastore <- save_to_duckdb(temp_dir)
+  data(vignette_switch_data)
+  expanded_duckdb_data <- save_expanded_data(datastore, subset(vignette_switch_data, trial_period %in% 1:12))
+
+  # check if omitting period reads in all data
+  expect_equal(nrow(read_expanded_data(expanded_duckdb_data)), 25948)
+
+  # check if period argument subsets data correctly
+  expect_equal(nrow(read_expanded_data(expanded_duckdb_data, 1)), 1979)
+  expect_equal(nrow(read_expanded_data(expanded_duckdb_data, c(5, 8, 12))), 6493)
+
+  # check if method throws an error when using a character as period
+  expect_error(read_expanded_data(expanded_csv_data, "1"))
+
+  # check if no new NAs are introduced
+  expect_equal(
+    sum(is.na.data.frame(read_expanded_data(expanded_duckdb_data))),
+    sum(is.na.data.frame(subset(vignette_switch_data, trial_period %in% 1:12)))
+  )
+
+  DBI::dbDisconnect(expanded_duckdb_data@con)
+  unlink(temp_dir, recursive = TRUE)
+})
