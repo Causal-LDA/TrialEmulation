a6dc2ef814459da913a023120bba5f9e6ecd4042
darkgoldenrod-cherry <julia.moesch@roche.com>
2024-07-02 12:53:56 +0200

    added te_outcome_data-class to te_data.R, added outcome_data slot to trial_sequence-class

--- R/te_data.R
+++ R/te_data.R
@@ -44,3 +44,34 @@ setMethod(
     print(object@data, nrows = 4, topn = 2)
   }
 )
+
+
+#' TrialEmulation Outcome Data Class
+#'
+#' @slot data A `data.table` object with columns "id", "period",
+#' @slot n_rows Number of rows
+#' @slot n_ids Number of IDs
+#' @slot periods Vector of periods
+#'   "treatment", "outcome", "eligible"
+setClass("te_outcome_data",
+         slots = c(
+           data = "data.table",
+           n_rows = "numeric",
+           n_ids = "numeric",
+           periods = "numeric"
+         )
+)
+
+setValidity(
+  "te_outcome_data",
+  function(object) {
+    checks <- list()
+    checks["cols_check"] <- check_names(
+      colnames(object@data),
+      must.include = c("id", "trial_period", "followup_time", "outcome", "weights")
+    )
+
+    msg <- unlist(lapply(checks, function(x) if (is.character(x)) x else NULL))
+    if (length(msg)) msg else TRUE
+  }
+)
