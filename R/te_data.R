0ffe554b3cbf3a5f3bdf4d2d92902824b2f0d2ac
darkgoldenrod-cherry <julia.moesch@roche.com>
2024-07-24 15:21:47 +0200

    styled te.data.R again

--- R/te_data.R
+++ R/te_data.R
@@ -88,7 +88,9 @@ setMethod(
       catn("No outcome data, use load_expanded_data()")
     } else {
       catn("Outcome data")
-      catn("N:", object@n_rows, "observations from", object@n_ids, "patients in", length(object@periods), "trial periods")
+      catn(
+        "N:", object@n_rows, "observations from", object@n_ids, "patients in", length(object@periods), "trial periods"
+      )
       catn("Periods:", object@periods)
       if (length(object@subset_condition)) catn("Subset condition:", object@subset_condition)
       if (length(object@p_control)) catn("P-control:", object@p_control)
